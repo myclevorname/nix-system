7f2c7d08707a835ab1c5a935f8d8ae0afeea65b9
Sam Connelly <myclevorname@gmail.com>
2024-07-22 11:14:23 -0400

    Teying to make this work

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "clevor's Configuration";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.llvm-ez80.url = "./llvm-ez80";
+  inputs.llvm-ez80.url = "git+file:///etc/nixos/llvm-ez80";
 
   # I will not use flake-utils as I want to reduce the amount of dependencies I have.
 
