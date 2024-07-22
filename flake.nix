e3793fb58736ee49d225b2d809c4638137e6dce0
Sam Connelly <myclevorname@gmail.com>
2024-07-22 11:23:51 -0400

    System 1
    Fixed`

--- flake.nix
+++ flake.nix
@@ -7,12 +7,12 @@
   # I will not use flake-utils as I want to reduce the amount of dependencies I have.
 
   outputs = { self, nixpkgs, llvm-ez80 }@attrs: {
-    nixosConfigurations."clevor-laptop-nixos" = nixpkgs.lib.nixosSystem {
+    nixosConfigurations."clevor-laptop-nixos" = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [
         ./configuration.nix
-        ({ config, pkgs, llvm-ez80, ... }: { environment.systemPackages = [ llvm-ez80 ]; })
+        ({ config, pkgs, llvm-ez80, ... }: { environment.systemPackages = [ llvm-ez80.packages.${system}.llvm-ez80 ]; })
       ];
     };
   };
