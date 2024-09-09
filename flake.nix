1cc1a2c8b0b5e12049d27235a2e55a285df5863c
Sam Connelly <myclevorname@gmail.com>
2024-09-09 17:25:47 -0400

    System 46: Move mg-lru into separate directory

--- flake.nix
+++ flake.nix
@@ -46,42 +46,7 @@
         ./network-configuration.nix
       ] ++ commonConfig;
     };
-    nixosModules.mg-lru = { config, lib, ... }: {
-      options = {
-        boot.kernel.mg-lru = {
-          enable = lib.mkEnableOption "the multi-gen LRU to improve performance under memory pressure";
-          leafPageTables = lib.mkOption {
-            default = true;
-            type = lib.types.bool;
-            description = ''
-              Whether to allow the multi-gen LRU to clear the accessed bit in leaf page table
-              entries in large batches.
-              Enabling it may theoretically worsen lock contention, but disabling it will
-              incur a minor performance penalty for the multi-gen LRU. Only disable if there
-              are unforeseen side effects.
-            '';
-          };
-          nonLeafPageTables = lib.mkOption {
-            default = true;
-            type = lib.types.bool;
-            description = ''
-              Whether to allow the multi-gen LRU to clear the accessed bit in non-leaf page
-              table entries.
-              Disabling it will incur a minor performance penalty for the multi-gen LRU. Only
-              disable if there are unforeseen side effects.
-            '';
-          };
-        };
-      };
-      config = lib.mkIf config.boot.kernel.mg-lru.enable {
-          boot.kernelParams = with config.boot.kernel.mg-lru;
-            let
-              enabled = if enable then 1 else 0;
-              leaf = if leafPageTables then 2 else 0;
-              nonLeaf = if nonLeafPageTables then 4 else 0;
-            in [ "CONFIG_LRU_GEN=y" "CONFIG_LRU_GEN_ENABLED=${builtins.toString (enabled+leaf+nonLeaf)}" ];
-        };
-    };
+    nixosModules.mg-lru = import ./modules/mg-lru.nix;
 
     nixosConfigurations."clevor-laptop-nixos-generic" = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
---
+++ modules/mg-lru.nix
@@ -0,0 +1,42 @@
+{ config, lib, ... }:
+{
+  options = {
+    boot.kernel.mg-lru = {
+      enable = lib.mkEnableOption "the multi-gen LRU to improve performance under memory pressure";
+      leafPageTables = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
+        description = ''
+          Whether to allow the multi-gen LRU to clear the accessed bit in leaf page table
+          entries in large batches.
+          Enabling it may theoretically worsen lock contention, but disabling it will
+          incur a minor performance penalty for the multi-gen LRU. Only disable if there
+          are unforeseen side effects.
+        '';
+      };
+      nonLeafPageTables = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
+        description = ''
+          Whether to allow the multi-gen LRU to clear the accessed bit in non-leaf page
+          table entries.
+          Disabling it will incur a minor performance penalty for the multi-gen LRU. Only
+          disable if there are unforeseen side effects.
+        '';
+      };
+    };
+  };
+  config = lib.mkIf config.boot.kernel.mg-lru.enable {
+    boot.kernelParams = with config.boot.kernel.mg-lru;
+      let
+        boolToInt = x: if x then 1 else 0;
+        enabled = boolToInt enable;
+        leaf = 2*(boolToInt leafPageTables);
+        nonLeaf = 4*(boolToInt nonLeafPageTables);
+      in
+        [
+          "CONFIG_LRU_GEN=y"
+          "CONFIG_LRU_GEN_ENABLED=${builtins.toString (enabled+leaf+nonLeaf)}"
+        ];
+  };
+}
